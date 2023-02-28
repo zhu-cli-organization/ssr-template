25ca42297bb97d6a40bbaa78430a5181a0a99120
zhangwenjing <zhangwenjing@hupu.com>
2023-02-28 14:32:38 +0800

    feat: 添加模板引擎

--- ask.js
+++ ask.js
@@ -1,1 +1,19 @@
 // 询问是否需要更新文件
+
+module.exports=[
+    {
+        type:'input', // input表示输入，confirm表示确认
+        name:'name',
+        message:'please input your project name',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'please input your project description',
+    },
+    {
+        type:'input',
+        name:'author',
+        message:'please input your project author',
+    }
+]
